9b1183cdb19ae509a11b6303834128c2e0f5aaf2
Candy González <candygonzalezdelgado@gmail.com>
2020-08-09 20:27:55 +0100

    Sintaxis del lenguaje C#

---
+++ Estructura.cs
@@ -0,0 +1,37 @@
+// Esqueleto de un programa en C#
+using System;
+namespace YourNamespace
+{
+    class YourClass
+    {
+    }
+
+    struct YourStruct
+    {
+    }
+
+    interface IYourInterface
+    {
+    }
+
+    delegate int YourDelegate();
+
+    enum YourEnum
+    {
+    }
+
+    namespace YourNestedNamespace
+    {
+        struct YourStruct
+        {
+        }
+    }
+
+    class YourMainClass
+    {
+        static void Main(string[] args)
+        {
+            //El código de mi programa empieza aquí...
+        }
+    }
+}
---
+++ HolaMundo.cs
@@ -0,0 +1,9 @@
+//using System;
+
+class miClase
+{
+    static void Main() {
+        //Aquí va nuestro código  
+        System.Console.WriteLine("Hola mundo cruel");
+    }
+}
